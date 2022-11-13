63f80db23ea50ab8e8576a49412d1ff79fab089c
Anastasyy <nastenysh.pa@gmail.com>
2022-11-14 00:28:25 +0300

    Added 2.js

--- src/org/itmo/lab2/BattlePok.java
+++ src/org/itmo/lab2/BattlePok.java
@@ -2,24 +2,26 @@ package org.itmo.lab2;
 import ru.ifmo.se.pokemon.*;
 class BattlePok{
 	public static void main(String[] args){
-		Battle b = new Battle();
-		Pokemon p1 = new Pokemon("Чужой",1);
-		Pokemon p2 = new Pokemon("Хищник",1);
-		Pokemon p3 = new Virision("Халк",2);
-		Pokemon p4 = new Scyther("Дикарь",2);
-		Pokemon p5 = new Scizor("Силач",1);
-		Pokemon p6 = new Happiny("Цветочек",2);
-		Pokemon p7 = new Blissey("Гуру",1);
-		Pokemon p8 = new Chansey("Ветерок",2);
-		b.addAlly(p1);
-		b.addFoe(p2);
-		b.addAlly(p3);
-		b.addFoe(p4);
-		b.addAlly(p5);
-		b.addFoe(p6);
-		b.addFoe(p7);
-		b.addAlly(p8);
-		b.go();
+		Battle battlePokemons = new Battle();
+		Pokemon onePok = new Pokemon("Чужой",1);
+		Pokemon twoPok = new Pokemon("Хищник",1);
+		Pokemon thrPok = new Virision("Халк",2);
+		Pokemon thoPok = new Scyther("Дикарь",2);
+		Pokemon fivPok = new Scizor("Силач",1);
+		Pokemon sixPok = new Happiny("Цветочек",2);
+		Pokemon sevPok = new Blissey("Гуру",1);
+		Pokemon eigPok = new Chansey("Ветерок",2);
+
+		battlePokemons.addAlly(onePok);
+		battlePokemons.addAlly(thrPok);
+		battlePokemons.addAlly(fivPok);
+		battlePokemons.addAlly(eigPok);
+
+		battlePokemons.addFoe(twoPok);
+		battlePokemons.addFoe(thoPok);
+		battlePokemons.addFoe(sixPok);
+		battlePokemons.addFoe(sevPok);
+		battlePokemons.go();
 
 
 
--- src/org/itmo/lab2/NewAttakcsPok.java
+++ src/org/itmo/lab2/NewAttakcsPok.java
@@ -4,151 +4,125 @@ class AerialAce extends SpecialMove{
 	public AerialAce(){
 		super(Type.GRASS,60,99999);
 	}
-	
 	@Override
 	protected java.lang.String describe(){
 		return "Мочит";
 	}
-
 	@Override
-    protected void applyOppDamage(Pokemon def, double damage){
-        Status PokCon = def.getCondition();
-        if (PokCon.equals(Status.BURN) || PokCon.equals(Status.POISON) ||PokCon.equals(Status.PARALYZE)) {
-            def.setMod(Stat.HP, (int) Math.round(damage) * 2);
-            Effect e = new Effect().chance(0.5).turns(3).stat(Stat.ATTACK, 1);
-            def.addEffect(e);
+    protected void applyOppDamage(Pokemon pokApplyOpp, double damagePok){ //сколько очков повреждения, настолько и ухудщается
+        Status pokCond = pokApplyOpp.getCondition();
+        if (pokCond.equals(Status.BURN) || pokCond.equals(Status.POISON) ||pokCond.equals(Status.PARALYZE)) { //условие то я ему увеличиваю здорове 
+            pokApplyOpp.setMod(Stat.HP, (int) Math.round(damagePok) * 2); //округляем
+            Effect newEff = new Effect().chance(0.5).turns(3).stat(Stat.ATTACK, 1); //через каждые три раза с верочтность 50% будет увлеичивтаться атка покмеона на одну ступень
+            pokApplyOpp.addEffect(newEff); 
         }
     }
-
 }
-
 class CalmMind extends SpecialMove{
 	public CalmMind(){
 		super(Type.ICE,15,567);
 	}
-	
 	@Override
 	protected java.lang.String describe(){
 	 	return "Мочит";
 	}
-
 }
-
-
 class GigaDrain extends SpecialMove{
 	public GigaDrain(){
 		super(Type.DRAGON,5,1000);
 	}
-	
 	@Override
 	protected java.lang.String describe(){
 	  return "Мочит";
 	}
-
 }
-
 class Leer extends SpecialMove{
 	public Leer(){
 		super(Type.GHOST,902,10);
 	}
-	
 	@Override
 	protected java.lang.String describe(){
 	  return "Мочит";
 	}
     @Override
-    protected void applyOppDamage(Pokemon def, double damage){
-        Status PokCon = def.getCondition();
-        if (PokCon.equals(Status.SLEEP) || PokCon.equals(Status.FREEZE) ||PokCon.equals(Status.NORMAL)) {
-            def.setMod(Stat.HP, (int) Math.round(damage) * 2);
-            Effect e = new Effect().chance(0.5).turns(3).stat(Stat.ATTACK, 4);
-            def.addEffect(e);
+    protected void applyOppDamage(Pokemon pokApplyOpp, double damagePok){
+        Status pokCond = pokApplyOpp.getCondition();
+        if (pokCond.equals(Status.SLEEP) || pokCond.equals(Status.FREEZE) ||pokCond.equals(Status.NORMAL)) {
+            pokApplyOpp.setMod(Stat.HP, (int) Math.round(damagePok) * 2);
+            Effect newEff = new Effect().chance(0.5).turns(3).stat(Stat.ATTACK, 4);
+            pokApplyOpp.addEffect(newEff);
         }
-
-    }      
+      }      
 }
 class Rest extends SpecialMove{
 	public Rest(){
 		super(Type.FAIRY,45,10);
 	}
-	
 	@Override
 	protected java.lang.String describe(){
 	  return "Летает";
 	}
-
 }
 class XScissor extends SpecialMove{
 	public XScissor(){
 		super(Type.FIRE,5,10);
 	}
-	
 	@Override
 	protected java.lang.String describe(){
 	  return "Летает";
 	}
-
 }
 class Venoshock extends SpecialMove{
 	public Venoshock(){
 		super(Type.ICE,53,102);
 	}
-	
 	@Override
 	protected java.lang.String describe(){
 	  return "Летает";
 	}
-
 }
 class FireBlast extends PhysicalMove{
 	public FireBlast(){
 		super(Type.GHOST,65,1021);
 	}
-	
 	@Override
 	protected java.lang.String describe(){
 	  return "Пуляется";
 	}
-
 }
 class Psychic extends PhysicalMove{
 	public Psychic(){
 		super(Type.GRASS,513,1042);
 	}
-	
 	@Override
 	protected java.lang.String describe(){
 	  return "Пуляется";
 	}
-
 }
 class MudBomb extends PhysicalMove{
 	public MudBomb(){
 		super(Type.BUG,33,142);
 	}
-	
 	@Override
 	protected java.lang.String describe(){
 	  return "Взрывается";
 	}
-
 }
 class Minimize extends PhysicalMove{
 	public Minimize(){
 		super(Type.NONE,312,1421);
 	}
-	
 	@Override
 	protected java.lang.String describe(){
 	  return "Взрывается";
 	}
 	@Override
-    protected void applyOppDamage(Pokemon def, double damage){
-        Status PokCon = def.getCondition();
-        if (PokCon.equals(Status.FREEZE) || PokCon.equals(Status.POISON) ||PokCon.equals(Status.SLEEP)) {
-            def.setMod(Stat.HP, (int) Math.round(damage) * 2);
-            Effect e = new Effect().chance(0.5).turns(3).stat(Stat.ATTACK, -4);
-            def.addEffect(e);
+    protected void applyOppDamage(Pokemon pokApplyOpp, double damagePok){
+        Status pokCond = pokApplyOpp.getCondition();
+        if (pokCond.equals(Status.FREEZE) || pokCond.equals(Status.POISON) ||pokCond.equals(Status.SLEEP)) {
+            pokApplyOpp.setMod(Stat.HP, (int) Math.round(damagePok) * 2);
+            Effect newEff = new Effect().chance(0.5).turns(3).stat(Stat.ATTACK, -4);
+            pokApplyOpp.addEffect(newEff);
         }
     }
 
